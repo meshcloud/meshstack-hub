4bf966db56c64071a411d660f7279075c3bc200c
Le <ule@meshcloud.io>
2025-03-26 10:03:05 +0100

    feat: redirect to `/all` when no specific path is provided
    CU-86c2ngww6

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: ':type', loadComponent: () => import('./features/template-gallery').then(m => m.TemplateGalleryComponent) },
-  { path: '', loadComponent: () => import('./features/template-gallery').then(m => m.TemplateGalleryComponent) }
+  { path: '', redirectTo: '/all', pathMatch: 'full' } 
 ];
