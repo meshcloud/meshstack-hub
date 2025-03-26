c88f370756ca27a60aa77a4fe89a1a48cbfad5eb
Le <ule@meshcloud.io>
2025-03-26 18:11:05 +0100

    feat: add search CU-86c2nh0x3

--- website/src/app/features/template-gallery/template-gallery.component.ts
+++ website/src/app/features/template-gallery/template-gallery.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription, forkJoin, map, take } from 'rxjs';
 
 import { PlatformType, Template } from 'app/core';
 import { CardComponent } from 'app/shared/card';
 import { NavigationComponent } from 'app/shared/navigation';
-import { PlatformLogoService } from 'app/shared/platform-logo';
+import { PlatformLogoData, PlatformLogoService } from 'app/shared/platform-logo';
 import { TemplateService } from 'app/shared/template';
 
 interface TemplateVm extends Template {
@@ -16,7 +17,7 @@ interface TemplateVm extends Template {
 
 @Component({
   selector: 'mst-template-gallery',
-  imports: [CommonModule, CardComponent, NavigationComponent],
+  imports: [CommonModule, ReactiveFormsModule, CardComponent, NavigationComponent],
   templateUrl: './template-gallery.component.html',
   styleUrl: './template-gallery.component.scss',
   standalone: true
@@ -25,22 +26,31 @@ export class TemplateGalleryComponent implements OnInit, OnDestroy {
 
   public templates$!: Observable<TemplateVm[]>;
 
+  public searchForm!: FormGroup;
+
   private routeSubscription!: Subscription;
+
+  private logos$!: Observable<PlatformLogoData>;
 
   constructor(
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     private templateService: TemplateService,
     private platformLogoService: PlatformLogoService
   ) { }
 
 
   public ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      searchTerm: ['']
+    });
+
     this.routeSubscription = this.route.paramMap.subscribe(params => {
       const type = params.get('type') as PlatformType ?? 'all';
-      const logos$ = this.platformLogoService.getLogoUrls();
+      this.logos$ = this.platformLogoService.getLogoUrls();
       this.templates$ = forkJoin({
         templates: this.templateService.filterTemplatesByPlatformType(type),
-        logos: logos$
+        logos: this.logos$
       })
         .pipe(
           map(({ templates, logos }) =>
@@ -58,4 +68,21 @@ export class TemplateGalleryComponent implements OnInit, OnDestroy {
     this.routeSubscription.unsubscribe();
   }
 
+  public onSearch(): void {
+    const search = this.searchForm.value.searchTerm;
+
+    this.templates$ = forkJoin({
+      templates: this.templateService.search(search),
+      logos: this.logos$
+    })
+      .pipe(
+        map(({ templates, logos }) =>
+          templates.map(item => ({
+            ...item,
+            imageUrl: logos[item.platformType] ?? null,
+            detailsRoute: `/template/${item.id}`
+          }))
+        )
+      );
+  }
 }
