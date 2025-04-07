2bf833b87f66bd5a3d397f7d2f37731477c576c1
Le <ule@meshcloud.io>
2025-04-07 16:40:59 +0200

    feat:  implement to open meshStack link to open in a new tab to use template
    * update dependencies
    * fix spellings and minor improvements
    
    CU-86c2nhgk4

--- website/src/app/features/template-details/import-dialog/import-dialog.component.ts
+++ website/src/app/features/template-details/import-dialog/import-dialog.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+interface ImportDialogForm {
+  meshStackUrl: FormControl<string>;
+}
 
 @Component({
   selector: 'mst-import-dialog',
@@ -10,25 +15,42 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ImportDialogComponent implements OnInit {
 
-  public form!: FormGroup;
+  @Input()
+  public name!: string;
 
-  public get meshStackUrl() {
-    return this.form.get('meshStackUrl')?.value;
-  }
+  @Input()
+  public modulePath!: string;
+
+  public form!: FormGroup<ImportDialogForm>;
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
     public activeModal: NgbActiveModal,
     private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      meshStackUrl: ['']
+      meshStackUrl: this.fb.nonNullable.control('', [Validators.required, Validators.pattern(/^(https?:\/\/).*/)]),
     });
   }
 
-  public open() {
-
+  public openMeshStackUrl() {
+    // Only runs in browser, not during SSR
+    if (isPlatformBrowser(this.platformId)) {
+      const url = new URL(this.getSanitizedMeshStackUrl());
+      url.searchParams.set('module-path', this.modulePath);
+      url.searchParams.set('name', this.name);
+      window.open(url.toString(), '_blank', 'noopener,noreferrer');
+    }
   }
 
+  private getSanitizedMeshStackUrl(): string {
+    let meshStackUrl = this.form.controls.meshStackUrl.value;
+      if (meshStackUrl.endsWith('/')) {
+        meshStackUrl = meshStackUrl.slice(0, -1);
+      }
+
+      return meshStackUrl;
+  }
 }
