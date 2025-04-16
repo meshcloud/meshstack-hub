df3290d50e617cb64d064091955d3fe7250564d1
Le <ule@meshcloud.io>
2025-04-16 14:54:55 +0200

    feat: store referrerUrl to localstorage * move logic to listen to message into app component

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -13,19 +13,44 @@ import { FooterComponent } from './shared/footer';
   templateUrl: './app.component.html',
   standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    this.loadPlausible();
+    // Ensure we are in the browser (not SSR)
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadPlausible();
+      this.setupMessageListener();
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.removeMessageListener();
   }
 
   public loadPlausible() {
-    // Ensure we are in the browser (not SSR)
+    const script = document.createElement('script');
+    script.src = 'https://plausible.cluster.dev.meshcloud.io/js/script.js';
+    script.setAttribute('data-domain', 'hub.meshcloud.io');
+    script.defer = true;
+    document.head.appendChild(script);
+  }
+
+  private setupMessageListener(): void {
     if (isPlatformBrowser(this.platformId)) {
-      const script = document.createElement('script');
-      script.src = 'https://plausible.cluster.dev.meshcloud.io/js/script.js';
-      script.setAttribute('data-domain', 'hub.meshcloud.io');
-      script.defer = true;
-      document.head.appendChild(script);
+      window.addEventListener('message', this.handleMessage.bind(this), false);
+    }
+  }
+
+  private removeMessageListener(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('message', this.handleMessage.bind(this), false);
+    }
+  }
+
+  private handleMessage(event: MessageEvent): void {
+    const originUrl = event.data.originUrl;
+
+    if (typeof originUrl === 'string') {
+      localStorage.setItem('referrerUrl', originUrl);
     }
   }
 }
