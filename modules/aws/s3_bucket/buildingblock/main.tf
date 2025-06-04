60d6f30d8765cb131ec1071b92ea449c8b82103c
Jelle den Burger <jdburger@meshcloud.io>
2025-06-04 09:03:28 +0200

    chore: Multi select demo

--- modules/aws/s3_bucket/buildingblock/main.tf
+++ modules/aws/s3_bucket/buildingblock/main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
   tags = {
-    "managed-by" = "meshStack"
+    for tag in var.tags :
+    split(":", tag)[0] => split(":", tag)[1]
   }
 }
