4c8d024c8aa4a1b12f14e3662cb94d9881e88933
Stefan Tomm <stomm@meshcloud.io>
2025-07-22 15:50:08 +0200

    feat: add optional repo owner to GitHub repo building block

--- modules/github/repository/buildingblock/main.tf
+++ modules/github/repository/buildingblock/main.tf
@@ -15,3 +15,10 @@ resource "github_repository" "repository" {
     }
   }
 }
+
+resource "github_repository_collaborator" "repo_owner" {
+  count      = var.repo_owner != null ? 1 : 0
+  repository = github_repository.repository.name
+  username   = var.repo_owner
+  permission = "admin"
+}
