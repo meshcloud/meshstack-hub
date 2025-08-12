c3e5b09ef4a2cab1d4ea9c8751aad71044f57e6b
Jo Schwandke <59925842+j0g3sc@users.noreply.github.com>
2025-08-12 10:01:38 +0200

    feat: Update provider.tf

--- modules/aks/github-connector/buildingblock/provider.tf
+++ modules/aks/github-connector/buildingblock/provider.tf
@@ -1,1 +1,13 @@
-provider "github" {}
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  app_auth {
+  }
+}
