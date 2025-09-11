12624156037b926873df966417cb5b8ad622984b
Mohammad Alhussan <malhussan@meshcloud.io>
2025-09-11 10:24:01 +0200

    feat: wif support for storage bucket BB

--- modules/gcp/storage-bucket/backplane/variables.tf
+++ modules/gcp/storage-bucket/backplane/variables.tf
@@ -8,3 +8,15 @@ variable "service_account_id" {
   type        = string
   default     = "buildingblock-storage-sa"
 }
+
+variable "workload_identity_federation" {
+  description = "Configuration for workload identity federation"
+  type = object({
+    workload_identity_pool_identifier = string // Identifier for the workload identity pool
+    audience                          = string // Audience for the OIDC tokens
+    issuer                            = string // OIDC issuer URL
+    subject                           = string // Subject for workload identity federation (e.g., system:serviceaccount:namespace:service-account-name)
+    subject_token_file_path           = string // Path to the file containing the OIDC token
+  })
+  default = null
+}
