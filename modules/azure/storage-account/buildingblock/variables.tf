b16633c541fb46e2100f120b567de1f8dadb638e
Patrick <pmoore@meshcloud.io>
2025-09-17 09:32:33 +0200

    corrected descriptions

--- modules/azure/storage-account/buildingblock/variables.tf
+++ modules/azure/storage-account/buildingblock/variables.tf
@@ -1,16 +1,16 @@
 variable "storage_account_name" {
   type        = string
   nullable    = false
-  description = "The name of the key vault. Must be unique across entire Azure Region, not just within a Subscription."
+  description = "The name of the storage account. Must be unique across entire Azure Region, not just within a Subscription."
 }
 
 variable "storage_account_resource_group_name" {
   type        = string
   nullable    = false
-  description = "The name of the resource group containing the key vault."
+  description = "The name of the resource group containing the storage account."
 }
 
 variable "location" {
   type        = string
-  description = "The location/region where the key vault is created."
+  description = "The location/region where the storage account is created."
 }
