bbe6df716633fb155019ff474bb52581c6882a50
Mohammad Alhussan <malhussan@meshcloud.io>
2025-09-26 14:07:56 +0200

    fix: typo in permission

--- modules/azure/storage-account/backplane/main.tf
+++ modules/azure/storage-account/backplane/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_role_definition" "buildingblock_deploy" {
       "Microsoft.Storage/storageAccounts/managementPolicies/read",
       "Microsoft.Storage/storageAccounts/managementPolicies/delete",
       "Microsoft.Storage/storageAccounts/objectReplicationPolicies/write",
-      "Microsoft.Storage/storageAccounts/objectReplicationPolicies/reade",
+      "Microsoft.Storage/storageAccounts/objectReplicationPolicies/read",
       "Microsoft.Storage/storageAccounts/objectReplicationPolicies/delete"
     ]
   }
