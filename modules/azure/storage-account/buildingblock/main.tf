f921e71cf1ac41094569e59bb030007aae28cc30
Mohammad Alhussan <malhussan@meshcloud.io>
2025-09-26 14:56:10 +0200

    fix: storage account name cannot contain dashes

--- modules/azure/storage-account/buildingblock/main.tf
+++ modules/azure/storage-account/buildingblock/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "storage_account_rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.storage_account_name}-${random_string.resource_code.result}"
+  name                     = "${var.storage_account_name}${random_string.resource_code.result}"
   resource_group_name      = azurerm_resource_group.storage_account_rg.name
   location                 = azurerm_resource_group.storage_account_rg.location
   account_tier             = "Standard"
