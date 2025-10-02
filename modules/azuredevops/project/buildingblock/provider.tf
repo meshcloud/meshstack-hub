db831ea0f271475d705b2a64cddf20c377fe6363
Young-Hwan <yhkim@meshcloud.io>
2025-10-02 14:53:30 +0200

    feauter: update azure devops provisioning

--- modules/azuredevops/project/buildingblock/provider.tf
+++ modules/azuredevops/project/buildingblock/provider.tf
@@ -12,3 +12,7 @@ provider "azuredevops" {
   org_service_url       = var.azure_devops_organization_url
   personal_access_token = data.azurerm_key_vault_secret.azure_devops_pat.value
 }
+provider "azurerm" {
+  features {}
+  # Configuration options
+}
