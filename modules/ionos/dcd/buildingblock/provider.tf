c7e6e13991af2b871b6940b5261ad056d4335b9f
Young-Hwan <yhkim@meshcloud.io>
2025-10-08 15:40:20 +0200

    feature: update ionos dcd building block

--- modules/ionos/dcd/buildingblock/provider.tf
+++ modules/ionos/dcd/buildingblock/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    ionoscloud = {
-      source  = "ionos-cloud/ionoscloud"
-      version = "~> 6.4.0"
-    }
-  }
-}
-
 provider "ionoscloud" {
   token = var.ionos_token
 }
