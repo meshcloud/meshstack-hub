2ab373351ce524ea217a40630a43e74ff751b6c6
Young-Hwan <yhkim@meshcloud.io>
2025-12-01 17:01:59 +0100

    feat: update output file

--- modules/meshstack/payment-method/buildingblock/outputs.tf
+++ modules/meshstack/payment-method/buildingblock/outputs.tf
@@ -1,19 +1,14 @@
-output "payment_method_id" {
-  value       = meshstack_payment_method.payment_method.id
-  description = "The ID of the created payment method"
-}
-
 output "payment_method_name" {
-  value       = meshstack_payment_method.payment_method.name
+  value       = meshstack_payment_method.payment_method.metadata.name
   description = "The name of the payment method"
 }
 
 output "workspace_id" {
-  value       = meshstack_payment_method.payment_method.workspace_id
+  value       = meshstack_payment_method.payment_method.metadata.owned_by_workspace
   description = "The workspace ID associated with this payment method"
 }
 
 output "amount" {
-  value       = meshstack_payment_method.payment_method.amount
+  value       = meshstack_payment_method.payment_method.spec.amount
   description = "The budget amount for this payment method"
 }
