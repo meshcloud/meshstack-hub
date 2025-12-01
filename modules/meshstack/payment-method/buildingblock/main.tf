429289be8c9e39c4165296b9d289361827893854
Young-Hwan <yhkim@meshcloud.io>
2025-12-01 16:51:03 +0100

    style: refactored

--- modules/meshstack/payment-method/buildingblock/main.tf
+++ modules/meshstack/payment-method/buildingblock/main.tf
@@ -1,12 +1,12 @@
 resource "meshstack_payment_method" "payment_method" {
   metadata = {
-    name = var.payment_method_name
-    owned_by_workspace = var.workspace_id 
+    name               = var.payment_method_name
+    owned_by_workspace = var.workspace_id
   }
   spec = {
-    display_name = var.payment_method_name
-    amount       = var.amount
+    display_name    = var.payment_method_name
+    amount          = var.amount
     expiration_date = var.expiration_date
-    tags = var.tags
+    tags            = var.tags
   }
 }
