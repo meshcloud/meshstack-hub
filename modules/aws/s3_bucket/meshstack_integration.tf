9e228528788c23698d2a0e7e1a35ffb39bd6733d
Andreas Grub <agrub@meshcloud.io>
2026-02-20 13:42:59 +0100

    feat: flatline backplane module to resolve circular dep

--- modules/aws/s3_bucket/meshstack_integration.tf
+++ modules/aws/s3_bucket/meshstack_integration.tf
@@ -2,10 +2,19 @@ locals {
   env                  = "demo"
   workspace_identifier = "m25-platform"
   region               = "eu-central-1"
+  issuer = "https://container.googleapis.com/v1/projects/meshcloud-meshcloud--bc0/locations/europe-west1/clusters/meshstacks-ha"
+  audience = "aws-workload-identity-provider:meshcloud-demo"
+  role_name = "BuildingBlockS3IdentityFederation-${random_string.name_suffix.result}"
 }
 
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.12.0"
+    }
     meshstack = {
       source  = "meshcloud/meshstack"
       version = "0.18.0"
@@ -15,27 +24,88 @@ terraform {
 
 # fill in your meshStack API endpoint and credentials here
 provider "meshstack" {
-  endpoint  = ""
-  apikey    = ""
-  apisecret = ""
 }
 
 provider "aws" {
   region = local.region
 }
 
-# Import the backplane module
-module "backplane" {
-  source = "github.com/meshcloud/meshstack-hub//modules/aws/s3_bucket/backplane?ref=main"
+# backplane
 
-  # bucket is provided with WIF setup, fill in your WIF configuration here
-  workload_identity_federation = {
-    issuer   = ""
-    audience = ""
-    subjects = [
-      "",
+resource "random_string" "name_suffix" {
+  length  = 4
+  special = false
+}
+
+data "aws_iam_policy_document" "s3_full_access" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
     ]
   }
+}
+
+resource "aws_iam_policy" "buildingblock_s3_policy" {
+  name        = "S3BuildingBlockFederatedPolicy-${random_string.name_suffix.result}"
+  description = "Policy for the S3 Building Block"
+  policy      = data.aws_iam_policy_document.s3_full_access.json
+}
+
+# Workload Identity Federation
+
+resource "aws_iam_openid_connect_provider" "buildingblock_oidc_provider" {
+  url            = local.issuer
+  client_id_list = [local.audience]
+}
+
+data "aws_iam_policy_document" "workload_identity_federation" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.buildingblock_oidc_provider.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${trimprefix(local.issuer, "https://")}:aud"
+
+      values = [local.audience]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "${trimprefix(local.issuer, "https://")}:sub"
+
+      values = ["system:serviceaccount:meshcloud-demo:workspace.${local.workspace_identifier}.buildingblockdefinition.${meshstack_building_block_definition.awsS3Bucket.metadata.uuid}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "assume_federated_role" {
+  name               = local.role_name
+  assume_role_policy = data.aws_iam_policy_document.workload_identity_federation.json
+}
+
+resource "aws_iam_role_policy_attachment" "buildingblock_s3" {
+  role       = aws_iam_role.assume_federated_role.name
+  policy_arn = aws_iam_policy.buildingblock_s3_policy.arn
+}
+
+
+output "workload_identity_federation_role" {
+  value = aws_iam_role.assume_federated_role.arn
+}
+
+output "definition_uuid" {
+  value = meshstack_building_block_definition.awsS3Bucket.metadata.uuid
 }
 
 # Import the building block definition into meshStack
@@ -78,7 +148,7 @@ resource "meshstack_building_block_definition" "awsS3Bucket" {
         description     = "The ARN of the AWS role to assume for provisioning the S3 bucket",
         assignment_type = "STATIC",
         is_environment  = true
-        argument        = jsonencode(module.backplane.workload_identity_federation_role)
+        argument        = jsonencode("arn:aws:iam::${split(":", aws_iam_openid_connect_provider.buildingblock_oidc_provider.arn)[4]}:role/${local.role_name}")
       },
       AWS_WEB_IDENTITY_TOKEN_FILE = {
         type            = "STRING",
