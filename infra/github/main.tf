46b262e2c3ecb22d839880a21594342547dd7269
Andreas Grub <agrub@meshcloud.io>
2026-03-11 13:06:09 +0100

    feat: import github repo ruleset into infra/github

--- infra/github/main.tf
+++ infra/github/main.tf
@@ -36,3 +36,43 @@ resource "github_actions_environment_secret" "deploy_key" {
   secret_name     = "REPO_DEPLOY_KEY"
   plaintext_value = tls_private_key.deploy_key.private_key_openssh
 }
+
+resource "github_repository_ruleset" "protect_default_branch" {
+  enforcement = "active"
+  name        = "Protect Default"
+  repository  = local.github_repository_name
+  target      = "branch"
+  bypass_actors {
+    actor_id    = 0
+    actor_type  = "DeployKey"
+    bypass_mode = "always"
+  }
+  bypass_actors {
+    actor_id    = 2
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+  conditions {
+    ref_name {
+      exclude = []
+      include = ["~DEFAULT_BRANCH"]
+    }
+  }
+  rules {
+    creation                      = false
+    deletion                      = true
+    non_fast_forward              = true
+    required_linear_history       = true
+    required_signatures           = false
+    update                        = false
+    update_allows_fetch_and_merge = false
+    pull_request {
+      allowed_merge_methods             = ["rebase"]
+      dismiss_stale_reviews_on_push     = false
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+  }
+}
