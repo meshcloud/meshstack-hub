4f41f6d60e33b49b45f3a922738106b7752635e7
Jo Schwandke <jschwandke@meshcloud.io>
2026-03-12 11:07:48 +0100

    refactor: re-iterate on stackit connector backplane CU-86c8j8hzh

--- modules/stackit/connector/backplane/main.tf
+++ modules/stackit/connector/backplane/main.tf
@@ -9,25 +9,3 @@
 # - update image pull secret
 # - deployment
 #
-
-
-# SPN for Terraform state
-# TODO
-
-# Container registry
-# We're using a shared container registry for all consumers of this building block.
-# We have a predefined harbor instance in stackit for it
-
-
-# The ClusterIssuer is needed so that SSL certificates can be issued for projects using the GitHub Actions Connector.
-resource "kubernetes_cluster_role" "clusterissuer_reader" {
-  metadata {
-    name = "clusterissuer-reader"
-  }
-
-  rule {
-    api_groups = ["cert-manager.io"]
-    resources  = ["clusterissuers"]
-    verbs      = ["get"]
-  }
-}
