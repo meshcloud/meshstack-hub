259eea2f1f08910be2179fdcb70014399f20b810
Florian Nowarre <fnowarre@meshcloud.io>
2026-03-17 16:14:33 +0100

    chore: remove locals

--- modules/azuredevops/project/buildingblock/outputs.tf
+++ modules/azuredevops/project/buildingblock/outputs.tf
@@ -15,39 +15,39 @@ output "project_visibility" {
   value       = azuredevops_project.main.visibility
 }
 
-output "user_assignments" {
-  description = "Map of users and their assigned roles"
-  value = {
-    for user in var.users : user.email => {
-      meshIdentifier = user.meshIdentifier
-      username       = user.username
-      firstName      = user.firstName
-      lastName       = user.lastName
-      euid           = user.euid
-      roles          = user.roles
-    }
-  }
-}
+# output "user_assignments" {
+#   description = "Map of users and their assigned roles"
+#   value = {
+#     for user in var.users : user.email => {
+#       meshIdentifier = user.meshIdentifier
+#       username       = user.username
+#       firstName      = user.firstName
+#       lastName       = user.lastName
+#       euid           = user.euid
+#       roles          = user.roles
+#     }
+#   }
+# }
 
-output "group_memberships" {
-  description = "Information about group memberships"
-  value = {
-    readers = {
-      group_descriptor = data.azuredevops_group.project_readers.descriptor
-      members          = local.readers
-    }
-    contributors = {
-      group_descriptor = data.azuredevops_group.project_contributors.descriptor
-      members          = local.contributors
-    }
-    administrators = {
-      group_descriptor = data.azuredevops_group.project_administrators.descriptor
-      members          = local.administrators
-    }
-  }
-}
+# output "group_memberships" {
+#   description = "Information about group memberships"
+#   value = {
+#     readers = {
+#       group_descriptor = data.azuredevops_group.project_readers.descriptor
+#       members          = local.readers
+#     }
+#     contributors = {
+#       group_descriptor = data.azuredevops_group.project_contributors.descriptor
+#       members          = local.contributors
+#     }
+#     administrators = {
+#       group_descriptor = data.azuredevops_group.project_administrators.descriptor
+#       members          = local.administrators
+#     }
+#   }
+# }
 
-output "project_features" {
-  description = "Enabled/disabled project features"
-  value       = var.project_features
-}
+# output "project_features" {
+#   description = "Enabled/disabled project features"
+#   value       = var.project_features
+# }
