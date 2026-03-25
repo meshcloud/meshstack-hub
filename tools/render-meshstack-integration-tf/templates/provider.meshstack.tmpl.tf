8d355e8aac9ebf694def753a7d4383749f0979fe
Andreas Grub <agrub@meshcloud.io>
2026-03-25 06:24:18 +0100

    fix: provider better provider templates with environment vars usage

--- tools/render-meshstack-integration-tf/templates/provider.meshstack.tmpl.tf
+++ tools/render-meshstack-integration-tf/templates/provider.meshstack.tmpl.tf
@@ -1,7 +1,7 @@
 provider "meshstack" {
-  endpoint = "https://api.my.meshstack.io"
-  # Configure meshStack API credentials here or use environment variables
+  # Configure using environment variables
+  # MESHSTACK_ENDPOINT=https://api.my.meshstack.io
+  # and
   # MESHSTACK_API_KEY and MESHSTACK_API_SECRET
-  # apikey    = "00000000-0000-0000-0000-000000000000"
-  # apisecret = "uFOu4OjbE4Ji..."
+  # See also https://docs.meshcloud.io/api/authentication/api-keys/
 }
