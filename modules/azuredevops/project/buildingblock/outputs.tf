1ce14dc68f22d35cffa64f4ba021d85b43f4eecb
Florian Nowarre <fnowarre@meshcloud.io>
2026-03-30 14:13:45 +0200

    chore: change project module

--- modules/azuredevops/project/buildingblock/outputs.tf
+++ modules/azuredevops/project/buildingblock/outputs.tf
@@ -49,38 +49,3 @@ output "project_features" {
   description = "Enabled/disabled project features"
   value       = var.project_features
 }
-
-output "repository_id" {
-  description = "ID of the created repository"
-  value       = azuredevops_git_repository.main.id
-}
-
-output "repository_name" {
-  description = "Name of the created repository"
-  value       = azuredevops_git_repository.main.name
-}
-
-output "repository_url" {
-  description = "URL of the created repository"
-  value       = azuredevops_git_repository.main.url
-}
-
-output "ssh_url" {
-  description = "SSH URL of the repository"
-  value       = azuredevops_git_repository.main.ssh_url
-}
-
-output "web_url" {
-  description = "Web URL of the repository"
-  value       = azuredevops_git_repository.main.web_url
-}
-
-output "default_branch" {
-  description = "Default branch of the repository"
-  value       = azuredevops_git_repository.main.default_branch
-}
-
-output "branch_policies_enabled" {
-  description = "Whether branch policies are enabled"
-  value       = var.enable_branch_policies
-}
--- modules/azuredevops/project/buildingblock/variables.tf
+++ modules/azuredevops/project/buildingblock/variables.tf
@@ -77,36 +77,3 @@ variable "users" {
     roles          = list(string)
   }))
 }
-
-variable "repository_name" {
-  description = "Name of the Git repository to create"
-  type        = string
-}
-
-variable "minimum_reviewers" {
-  description = "Minimum number of reviewers required for pull requests"
-  type        = number
-  default     = 1
-
-  validation {
-    condition     = var.minimum_reviewers >= 1 && var.minimum_reviewers <= 10
-    error_message = "minimum_reviewers must be between 1 and 10"
-  }
-}
-
-variable "enable_branch_policies" {
-  description = "Enable branch protection policies on the default branch"
-  type        = bool
-  default     = true
-}
-
-variable "init_type" {
-  description = "Type of repository initialization. Options: Clean, Import, Uninitialized"
-  type        = string
-  default     = "Clean"
-
-  validation {
-    condition     = contains(["Clean", "Import", "Uninitialized"], var.init_type)
-    error_message = "init_type must be one of: Clean, Import, Uninitialized"
-  }
-}
