cd2b6b3f47e071c1364fd544af32065fa9bb3516
Florian Nowarre <fnowarre@meshcloud.io>
2026-04-24 15:26:31 +0200

    feat: add costum roles

--- modules/azure/service-principal/buildingblock/outputs.tf
+++ modules/azure/service-principal/buildingblock/outputs.tf
@@ -19,8 +19,8 @@ output "service_principal_object_id" {
 }
 
 output "client_secret" {
-  description = "Client secret for the service principal (\"null\" if create_client_secret is false)"
-  value       = var.create_client_secret ? azuread_application_password.main[0].value : "null"
+  description = "Client secret for the service principal (null if create_client_secret is false)"
+  value       = var.create_client_secret ? azuread_application_password.main[0].value : null
   sensitive   = true
 }
 
@@ -34,14 +34,34 @@ output "subscription_id" {
   value       = data.azurerm_subscription.target.subscription_id
 }
 
-output "azure_role" {
-  description = "Azure role assigned to the service principal"
-  value       = var.azure_role
+output "role_assignment_id" {
+  description = "ID of the role assignment (null if no role was assigned)"
+  value       = length(azurerm_role_assignment.main) > 0 ? azurerm_role_assignment.main[0].id : null
+}
+
+output "role_name" {
+  description = "Name of the role assigned to the service principal (built-in or custom)"
+  value       = var.custom_role != null ? var.custom_role.name : var.azure_role
+}
+
+output "custom_role_id" {
+  description = "ID of the custom role definition (null if using built-in role)"
+  value       = var.custom_role != null ? azurerm_role_definition.custom[0].role_definition_resource_id : null
+}
+
+output "custom_role" {
+  description = "Custom role definition details (null if using built-in role)"
+  value = var.custom_role != null ? {
+    id          = azurerm_role_definition.custom[0].role_definition_resource_id
+    name        = azurerm_role_definition.custom[0].name
+    description = azurerm_role_definition.custom[0].description
+    scope       = azurerm_role_definition.custom[0].scope
+  } : null
 }
 
 output "secret_expiration_date" {
-  description = "Date when the service principal secret will expire (\"null\" if create_client_secret is false)"
-  value       = var.create_client_secret ? azuread_application_password.main[0].end_date : "null"
+  description = "Date when the service principal secret will expire (null if create_client_secret is false)"
+  value       = var.create_client_secret ? azuread_application_password.main[0].end_date : null
 }
 
 output "authentication_method" {
