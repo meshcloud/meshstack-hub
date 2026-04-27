c54798209ee218ab45023abcab9cc2c40550c4a8
Young-Hwan <yhkim@meshcloud.io>
2026-04-27 09:24:42 +0200

    fix: each contact can be set separate. Updated the backplane

--- modules/aws/alternate-contacts/buildingblock/outputs.tf
+++ modules/aws/alternate-contacts/buildingblock/outputs.tf
@@ -1,6 +1,8 @@
-output "contacts" {
+output "operations_contacts" {
   description = "Map of configured alternate contact types to their email addresses"
   value = {
-    for type, contact in aws_account_alternate_contact.this : type => contact.email_address
-  }
+    "operations" = aws_account_alternate_contact.operations.email_address
+    "billing"    = aws_account_alternate_contact.billing.email_address
+    "security"   = aws_account_alternate_contact.security.email_address
+    }
 }
