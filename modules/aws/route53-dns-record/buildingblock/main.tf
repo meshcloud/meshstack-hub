716a94704fb20a1dcd1c536657735138605105cc
Florian Nowarre <fnowarre@meshcloud.io>
2026-04-28 09:57:03 +0200

    chore: allow multiple values for NS records

--- modules/aws/route53-dns-record/buildingblock/main.tf
+++ modules/aws/route53-dns-record/buildingblock/main.tf
@@ -7,6 +7,12 @@ locals {
   # meshStack doesn't support empty strings as inputs, so we treat @ (which is common to denote apex records
   # in zonefiles) as a special value to indicate "empty"
   record_name = var.sub == "@" ? "" : var.sub
+
+  # For NS records, support comma-separated values (e.g., "ns1.example.com,ns2.example.com")
+  # For other record types, use the single value as-is
+  record_values = var.type == "NS" ? [
+    for v in split(",", var.record) : trimspace(v)
+  ] : [var.record]
 }
 
 resource "aws_route53_record" "record" {
@@ -14,5 +20,5 @@ resource "aws_route53_record" "record" {
   name    = join(".", compact([local.record_name, data.aws_route53_zone.zone.name]))
   type    = var.type
   ttl     = parseint(var.ttl, 10)
-  records = [var.record]
+  records = local.record_values
 }
