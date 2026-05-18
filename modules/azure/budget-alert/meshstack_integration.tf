93dec54c055787f0dcae2b9258f113e5a9a0a285
Johannes Rudolph <jrudolph@meshcloud.io>
2026-05-18 18:31:28 +0200

    feat: use hub.git_ref for backplane source
    This keeps backplane and buildingblock implementation versions aligned.
    Only downside is it requires variables during `tofu init` and iirc it's a
    feature that's only supported in opentofu, not in terraform.

--- modules/azure/budget-alert/meshstack_integration.tf
+++ modules/azure/budget-alert/meshstack_integration.tf
@@ -56,7 +56,7 @@ output "building_block_definition" {
 data "meshstack_integrations" "integrations" {}
 
 module "backplane" {
-  source = "github.com/meshcloud/meshstack-hub//modules/azure/budget-alert/backplane?ref=main"
+  source = "github.com/meshcloud/meshstack-hub//modules/azure/budget-alert/backplane?ref=${var.hub.git_ref}"
 
   name  = var.backplane_name
   scope = var.azure_scope
