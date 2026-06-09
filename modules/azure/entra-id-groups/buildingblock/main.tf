fa35fcca0d21953335275c224790184c2d3d177c
Young-Hwan <yhkim@meshcloud.io>
2026-06-09 10:10:22 +0200

    fix: adding user based on UPN not GUID

--- modules/azure/entra-id-groups/buildingblock/main.tf
+++ modules/azure/entra-id-groups/buildingblock/main.tf
@@ -3,6 +3,8 @@ locals {
   au_id      = var.administrative_unit_id != "" ? var.administrative_unit_id : null
   name_parts = compact([var.prefix, var.workspace_identifier, var.project_identifier])
 
+  unique_user_euids = toset([for user in var.users : user.euid])
+
   user_role_assignments = {
     for pair in flatten([
       for user in var.users : [
@@ -15,6 +17,11 @@ locals {
     ]) : pair.key => pair
     if contains(local.roles, pair.role)
   }
+}
+
+data "azuread_user" "this" {
+  for_each            = local.unique_user_euids
+  user_principal_name = each.value
 }
 
 resource "azuread_group" "project_role" {
@@ -36,5 +43,5 @@ resource "azuread_group_member" "project_role" {
   for_each = local.user_role_assignments
 
   group_object_id  = azuread_group.project_role[each.value.role].object_id
-  member_object_id = each.value.euid
+  member_object_id = data.azuread_user.this[each.value.euid].object_id
 }
