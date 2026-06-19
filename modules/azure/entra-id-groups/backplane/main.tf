a4927024fae48bd85ea4f6fd5855fa36fc66e8d7
Florian Nowarre <fnowarre@meshcloud.io>
2026-06-19 10:26:46 +0200

    chore: solve comments
    chore: solve comments

--- modules/azure/entra-id-groups/backplane/main.tf
+++ modules/azure/entra-id-groups/backplane/main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "backplane" {
+resource "azurerm_resource_group" "this" {
   name     = var.name
   location = var.location
 }
 
-resource "azurerm_user_assigned_identity" "backplane" {
+resource "azurerm_user_assigned_identity" "this" {
   name                = var.name
   location            = var.location
-  resource_group_name = azurerm_resource_group.backplane.name
+  resource_group_name = azurerm_resource_group.this.name
 }
 
-resource "azurerm_federated_identity_credential" "backplane" {
+resource "azurerm_federated_identity_credential" "this" {
   for_each = { for i, s in var.workload_identity_federation.subjects : tostring(i) => s }
 
   name                = "subject-${each.key}"
-  resource_group_name = azurerm_resource_group.backplane.name
-  parent_id           = azurerm_user_assigned_identity.backplane.id
+  resource_group_name = azurerm_resource_group.this.name
+  parent_id           = azurerm_user_assigned_identity.this.id
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.workload_identity_federation.issuer
   subject             = each.value
@@ -27,18 +27,18 @@ data "azuread_service_principal" "msgraph" {
 
 resource "azuread_app_role_assignment" "user_read_all" {
   app_role_id         = data.azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
-  principal_object_id = azurerm_user_assigned_identity.backplane.principal_id
+  principal_object_id = azurerm_user_assigned_identity.this.principal_id
   resource_object_id  = data.azuread_service_principal.msgraph.object_id
 }
 
 resource "azuread_app_role_assignment" "group_readwrite_all" {
   app_role_id         = data.azuread_service_principal.msgraph.app_role_ids["Group.ReadWrite.All"]
-  principal_object_id = azurerm_user_assigned_identity.backplane.principal_id
+  principal_object_id = azurerm_user_assigned_identity.this.principal_id
   resource_object_id  = data.azuread_service_principal.msgraph.object_id
 }
 
 resource "azuread_app_role_assignment" "administrative_unit_readwrite_all" {
   app_role_id         = data.azuread_service_principal.msgraph.app_role_ids["AdministrativeUnit.ReadWrite.All"]
-  principal_object_id = azurerm_user_assigned_identity.backplane.principal_id
+  principal_object_id = azurerm_user_assigned_identity.this.principal_id
   resource_object_id  = data.azuread_service_principal.msgraph.object_id
 }
