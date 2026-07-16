90c9bd9847dd0f04065e79c06fc29284a85c6420
Mohammad Alhussan <malhussan@meshcloud.io>
2026-07-16 17:19:25 +0200

    feat: bump azure-vm BBDs to OpenTofu 1.11.5
    Enables the meshstack_building_block_v2 -> meshstack_building_block moved block:
    cross-type moves need OpenTofu >= 1.10 (1.9 errors "Resource type mismatch"),
    and the provider implements the move since v0.23.0.

--- modules/azure/azure-virtual-machine/meshstack_integration.tf
+++ modules/azure/azure-virtual-machine/meshstack_integration.tf
@@ -122,7 +122,7 @@ resource "meshstack_building_block_definition" "this" {
 
     implementation = {
       terraform = {
-        terraform_version              = "1.9.0"
+        terraform_version              = "1.11.5"
         repository_url                 = "https://github.com/meshcloud/meshstack-hub.git"
         repository_path                = "modules/azure/azure-virtual-machine/buildingblock"
         ref_name                       = var.hub.git_ref
